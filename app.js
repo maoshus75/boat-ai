143279909d2541b35234c375bb70e80e2891e790
maoshus75 <maoshus75@gmail.com>
2026-08-23 09:22:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -186,4 +186,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   });
 
-});
+});const autoRaces = ["戸田 10R", "平和島 8R", "江戸川 11R"];
+
+let autoIndex = 0;
+
+setInterval(() => {
+
+  autoIndex = (autoIndex + 1) % autoRaces.length;
+
+  renderRace(autoRaces[autoIndex]);
+
+}, 5000);
