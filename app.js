960dfa54eab7bcc9e4bcc1e4a935f57160711f19
maoshus75 <maoshus75@gmail.com>
2026-08-24 09:07:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -438,10 +438,8 @@ async function loadRealData() {
 
     const data = await response.json();
 
-     alert("API取得成功\n" + JSON.stringify(data).slice(0, alert(JSON.stringify(data.programs.stadiums["3"].races["1"].racers["1"], null, 2));
+     alert("API取得成功\n" + JSON.stringify(data).slice(0, 1000));
 
-     
-     
      console.log("今日のレースデータ:", data);
 
    
